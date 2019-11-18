1a64b0da6c7bf1dfc4ed4585d0ed02e9afef46d2
quintin <qhdwight@gmail.com>
2019-11-18 14:29:51 -0800

    Separate hello world linux and macos

---
+++ hello_world_linux.s
@@ -0,0 +1,23 @@
+.data
+_message:
+	.string "Hello World!\n"
+
+.text
+.intel_syntax noprefix
+.globl main	
+main:
+	push rbp
+	mov	rbp, rsp
+
+	xor rcx, rcx
+	lea	rax, [rip + _message]
+	mov rsi, rax # Pointer to string
+	mov rdx, 14 # Size
+	mov rax, 1 # Write
+	mov rdi, 1 # Standard output
+	syscall
+
+	mov	eax, 0
+	pop	rbp
+	ret
+
